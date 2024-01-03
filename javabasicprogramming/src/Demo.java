e698bfcbd116c77b9919623cf921fdfb844f8974
Chetan Patel <155350722+ChetanPatel1999@users.noreply.github.com>
2024-01-03 10:44:45 +0530

    03/01

--- javabasicprogramming/src/Demo.java
+++ javabasicprogramming/src/Demo.java
@@ -3,6 +3,7 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("hi from demo class");
         System.out.println("hi from demo class");
+        System.out.println("hi from demo class");
     }
   
 }
